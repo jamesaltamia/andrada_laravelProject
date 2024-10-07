da4e3f932c59db356dae0c9294d0e4f1a0bdf7ec
jamesaltamia <jamesaltamia23@gmai.com>
2024-10-07 18:42:57 +0800

    text

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,9 +8,12 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                    <h3 class="text-red-700 font-bolder">Hello, World</h3>
+                    <p class="bg-blue-400 ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia, ad. Molestias voluptas iure placeat distinctio. Minus incidunt similique modi voluptatem!</p>
                 </div>
+
             </div>
         </div>
     </div>
